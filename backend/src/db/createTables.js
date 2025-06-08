43fd6082bbef2c13306501c74d0341bea80744f1
CarlosDR <dimatteoca@gmail.com>
2025-06-08 17:03:19 -0400

    createTables modified for createtable function and table ordered

--- backend/src/db/createTables.js
+++ backend/src/db/createTables.js
@@ -1,13 +1,43 @@
 //version de SQL mayor a 13.
-export const createMainTables = [
+
+import { pool } from './configDB.js';
+import pc from 'picocolors';
+
+//-----------------------------
+// initialization control table
+// export const initFlagTable = {
+//   table: `
+//     CREATE TABLE IF NOT EXISTS app_initialization (
+//       id SERIAL PRIMARY KEY,
+//       tables_created BOOLEAN NOT NULL DEFAULT FALSE,
+//       initialized_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+//       last_updated TIMESTAMPTZ NOT NULL DEFAULT NOW()
+//     );
+//   `,
+//   tblName: 'app_initialization',
+// };
+
+export const mainTables = [
+  // {
+  //   tblName: 'users',
+  //   table: `CREATE TABLE IF NOT EXISTS users(user_id UUID PRIMARY KEY NOT NULL, username VARCHAR(50) UNIQUE NOT NULL, email VARCHAR(50) UNIQUE NOT NULL, user_firstname VARCHAR(25) NOT NULL,  user_lastname VARCHAR(25)  NOT NULL, user_contact VARCHAR(25), password_hashed VARCHAR(255), currency_id INT REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE,google_id VARCHAR(255) UNIQUE, display_name VARCHAR(255), auth_method VARCHAR(50) DEFAULT 'password',created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,user_role_id INT  REFERENCES user_roles(user_role_id) ON DELETE SET NULL ON UPDATE CASCADE) `,
+  // },
+
   {
-    tblName: 'users',
-    table: `CREATE TABLE IF NOT EXISTS users(user_id UUID PRIMARY KEY NOT NULL, username VARCHAR(50) UNIQUE NOT NULL, email VARCHAR(50) UNIQUE NOT NULL, user_firstname VARCHAR(25) NOT NULL,  user_lastname VARCHAR(25)  NOT NULL, user_contact VARCHAR(25), password_hashed VARCHAR(255), currency_id INT REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE,google_id VARCHAR(255) UNIQUE, display_name VARCHAR(255), auth_method VARCHAR(50) DEFAULT 'password',created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,user_role_id INT  REFERENCES user_roles(user_role_id) ON DELETE SET NULL ON UPDATE CASCADE) `,
+    table: `
+    CREATE TABLE IF NOT EXISTS app_initialization (
+      id SERIAL PRIMARY KEY,
+      tables_created BOOLEAN NOT NULL DEFAULT FALSE,
+      initialized_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+      updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+    );
+  `,
+    tblName: 'app_initialization',
   },
 
   {
     tblName: 'user_accounts',
-    table: `CREATE TABLE  IF NOT EXISTS user_accounts (account_id SERIAL PRIMARY KEY NOT NULL, 
+    table: `CREATE TABLE IF NOT EXISTS user_accounts (account_id SERIAL PRIMARY KEY NOT NULL, 
 user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE ON UPDATE CASCADE,
 account_name VARCHAR(50) NOT NULL,
 account_type_id INT  REFERENCES account_types(account_type_id) ON DELETE SET NULL ON UPDATE CASCADE, 
@@ -22,17 +52,17 @@ currency_id INT NOT NULL REFERENCES currencies(currency_id) ON DELETE RESTRICT O
 
   //CHECK (account_start_date <= NOW()
   //------specific type accounts
-  {
-    tblName: 'bank_accounts',
-    table:
-      'CREATE TABLE IF NOT EXISTS bank_accounts(account_id INT PRIMARY KEY REFERENCES user_accounts(account_id), account_starting_amount DECIMAL (15,2),currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, account_start_date TIMESTAMP NOT NULL )',
-  },
+  // {
+  //   tblName: 'bank_accounts',
+  //   table:
+  //     'CREATE TABLE IF NOT EXISTS bank_accounts(account_id INT PRIMARY KEY REFERENCES user_accounts(account_id), account_starting_amount DECIMAL (15,2),currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, account_start_date TIMESTAMP NOT NULL )',
+  // },
 
-  {
-    tblName: 'investment_accounts',
-    table:
-      'CREATE TABLE IF NOT EXISTS investment_accounts(account_id INT PRIMARY KEY REFERENCES user_accounts(account_id), account_starting_amount DECIMAL (15,2),currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, account_start_date TIMESTAMP NOT NULL)',
-  },
+  // {
+  //   tblName: 'investment_accounts',
+  //   table:
+  //     'CREATE TABLE IF NOT EXISTS investment_accounts(account_id INT PRIMARY KEY REFERENCES user_accounts(account_id), account_starting_amount DECIMAL (15,2),currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, account_start_date TIMESTAMP NOT NULL)',
+  // },
 
   {
     tblName: 'income_source_accounts',
@@ -83,7 +113,7 @@ currency_id INT NOT NULL REFERENCES currencies(currency_id) ON DELETE RESTRICT O
 
   },
   */
- 
+
   // {
   //   tblName: 'movement_types',
   //   table: `CREATE TABLE IF NOT EXISTS movement_types (movement_type_id SERIAL PRIMARY KEY NOT NULL, movement_type_name VARCHAR(15) NOT NULL CHECK (movement_type_name IN ('expense', 'income', 'investment', 'debt', 'pocket', 'transfer', 'receive','account-opening')))`,
@@ -96,16 +126,16 @@ currency_id INT NOT NULL REFERENCES currencies(currency_id) ON DELETE RESTRICT O
   //   account_id INT NOT NULL REFERENCES user_accounts(account_id) ON DELETE CASCADE ON UPDATE CASCADE)`,
   // },
 
-  {
-    tblName: 'expense_categories',
-    table: `CREATE TABLE IF NOT EXISTS expense_categories( 
-    category_id SERIAL PRIMARY KEY NOT NULL,
-    category_name VARCHAR(50) NOT NULL,
-    nature_name VARCHAR(8) NOT NULL CHECK (nature_name IN ('must', 'need', 'other', 'want')),
-    budget DECIMAL(15,2) NOT NULL, 
-    currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE, 
-)`,
-  },
+  //   {
+  //     tblName: 'expense_categories',
+  //     table: `CREATE TABLE IF NOT EXISTS expense_categories(
+  //     category_id SERIAL PRIMARY KEY NOT NULL,
+  //     category_name VARCHAR(50) NOT NULL,
+  //     nature_name VARCHAR(8) NOT NULL CHECK (nature_name IN ('must', 'need', 'other', 'want')),
+  //     budget DECIMAL(15,2) NOT NULL,
+  //     currency_id INT  REFERENCES currencies(currency_id) ON DELETE SET NULL ON UPDATE CASCADE,
+  // )`,
+  //   },
 
   //   {
   //     tblName: 'income_movements',
@@ -130,14 +160,14 @@ currency_id INT NOT NULL REFERENCES currencies(currency_id) ON DELETE RESTRICT O
   //     debtor_id INT NOT NULL
   //  )`,
   //   },
-  {
-    tblName: 'debt_debtors',
-    table: `CREATE TABLE IF NOT EXISTS debt_debtors (
-    debtor_id SERIAL PRIMARY KEY NOT NULL,
-    debtor_name VARCHAR(25) NOT NULL,
-    debtor_lastname VARCHAR(25) NOT NULL
-    )`,
-  },
+  // {
+  //   tblName: 'debt_debtors',
+  //   table: `CREATE TABLE IF NOT EXISTS debt_debtors (
+  //   debtor_id SERIAL PRIMARY KEY NOT NULL,
+  //   debtor_name VARCHAR(25) NOT NULL,
+  //   debtor_lastname VARCHAR(25) NOT NULL
+  //   )`,
+  // },
   //   {
   //     tblName: 'pocket_movements',
   //     table: `CREATE TABLE IF NOT EXISTS pocket_movements (
@@ -152,41 +182,41 @@ currency_id INT NOT NULL REFERENCES currencies(currency_id) ON DELETE RESTRICT O
   {
     tblName: 'transactions',
     table: `CREATE TABLE IF NOT EXISTS transactions(
-transaction_id SERIAL PRIMARY KEY,
-user_id UUID NOT NULL,
-description TEXT,
-amount DECIMAL(15,2) NOT NULL, 
-movement_type_id INTEGER NOT NULL,
-transaction_type_id INTEGER NOT NULL,
-currency_id INTEGER NOT NULL, 
-account_id INTEGER NOT NULL,
- source_account_id INT  ,
-destination_account_id INT  , 
-status VARCHAR(50) NOT NULL, 
-transaction_actual_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP NOT NULL DEFAULT 
-)`,
+      transaction_id SERIAL PRIMARY KEY,
+      user_id UUID NOT NULL,
+      description TEXT,
+      amount DECIMAL(15,2) NOT NULL, 
+      movement_type_id INTEGER NOT NULL,
+      transaction_type_id INTEGER NOT NULL,
+      currency_id INTEGER NOT NULL, 
+      account_id INTEGER NOT NULL,
+      source_account_id INT  ,
+      destination_account_id INT  , 
+      status VARCHAR(50) NOT NULL, 
+      transaction_actual_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )`,
   },
 
   {
     tblName: 'refresh_tokens',
     table: `
-CREATE TABLE IF NOT EXISTS refresh_tokens (
-token_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
-token TEXT NOT NULL UNIQUE,
-expiration_date TIMESTAMP NOT NULL,
-revoked BOOLEAN DEFAULT FALSE,
-user_agent TEXT,
-ip_address TEXT, 
-created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
+      CREATE TABLE IF NOT EXISTS refresh_tokens (
+      token_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+      user_id UUID NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+      token TEXT NOT NULL UNIQUE,
+      expiration_date TIMESTAMP NOT NULL,
+      revoked BOOLEAN DEFAULT FALSE,
+      user_agent TEXT,
+      ip_address TEXT, 
+      created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+      updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+      )
 `,
   },
 ];
 
-//REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE
+//=============================================
 
 export const createSearchIndexes = [
   {
@@ -205,5 +235,54 @@ export const createSearchIndexes = [
 ];
 
 // ===========================
-// source_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE,
-// destination_account_id INT  REFERENCES user_accounts(account_id) ON DELETE SET NULL ON UPDATE CASCADE,
+// Función para crear la tabla de inicialización
+// export async function createInitFlagTable() {
+//   try {
+//     await pool.query(initFlagTable);
+//     console.log('Initialization flag table created/verified');
+//   } catch (error) {
+//     console.error('Error creating initialization flag table:', error);
+//     throw error;
+//   }
+// }
+
+//----
+//Create main tables needed at initialization of the app
+export async function createTables() {
+  try {
+    console.log('Creando las tablas en caso que no existan...');
+    await Promise.allSettled(
+      mainTables.map(async (item, ind) => {
+        try {
+          await pool.query(item.table);
+          // console.log(ind, item.tblName, 'verified/created');
+          console.log(
+            pc.green(`${ind}) Table ${item.tblName} verified/created`)
+          );
+          // console.log(pc.green(`Table ${tblName} verified/created`));
+        } catch (error) {
+          console.error(pc.red(`Error creating table ${item.tblName}:`, error));
+          throw error;
+        }
+      })
+    ).then((results) => {
+      results.forEach((result, indx) => {
+        if (result.status === 'fulfilled') {
+          console.log(
+            `Table ${mainTables[indx].tblName} was successfully created .`
+          );
+        } else if (result.status === 'rejected') {
+          console.error(
+            `Table ${mainTables[indx].tblName} failed to create:`,
+            result.reason
+          );
+        }
+      });
+    });
+  } catch (error) {
+    console.error(pc.red('Error in table creation process:'), error);
+    throw error;
+  }
+}
+
+//============================
