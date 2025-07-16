fa46fa4f9a11adcb9c85b0cb4e4aca5e37b705f6
CarlosDR <dimatteoca@gmail.com>
2025-07-16 13:45:28 -0400

    Debtors fixed

---
+++ frontend/src/pages/debts/Debtors.tsx
@@ -0,0 +1,43 @@
+import { NavigateFunction, useLocation, useNavigate } from 'react-router-dom';
+import { CardTitle } from '../../general_components/CardTitle';
+import OpenAddEditBtn from '../../general_components/OpenAddEditBtn';
+import ListOfDebtors from './components/ListOfDebtors';
+
+function Debtors() {
+  const originRoute = useLocation().pathname;
+  const navigateTo: NavigateFunction = useNavigate();
+  // console.log(originRoute);
+
+  //functions
+  const createNewProfile = (originRoute: string) => {
+    navigateTo(originRoute + '/new_profile', {
+      state: { previousRoute: originRoute },
+    });
+  };
+
+  return (
+    <>
+      <section className='content__presentation'>
+        <div className='debts cards__presentation '>
+          <OpenAddEditBtn
+            btnFunction={createNewProfile}
+            btnFunctionArg={originRoute}
+            btnPreviousRoute={originRoute}
+          >
+            <div className='open__btn__label'>New Debtor</div>
+          </OpenAddEditBtn>
+          
+          <CardTitle>Summary</CardTitle>
+
+          <ListOfDebtors
+            previousRoute={originRoute}
+            accountType={'debtor'}
+          
+          ></ListOfDebtors>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default Debtors;
