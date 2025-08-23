b5d44f04ab7272d92990db2641988aef6b2287f0
CarlosDR <dimatteoca@gmail.com>
2025-08-23 10:06:48 -0400

    functions fixed

--- frontend/src/helpers/functions.ts
+++ frontend/src/helpers/functions.ts
@@ -47,7 +47,6 @@ export function getCurrencySymbol(chosenCurrency = 'USD') {
     return chosenCurrency;
   }
 }
-
 //--------------------
 export function digitRound(n = Number.MIN_VALUE, digit = 2) {
   return Math.round(n * Math.pow(10, digit)) / Math.pow(10, digit);
@@ -288,32 +287,3 @@ export const statusFn = (
   return type;
 };
 //-------------------------------
-//--customize toast message notifictions
-import {toast, ToastOptions, TypeOptions} from 'react-toastify'
-
-type StatusToastConfig = {
-  color:string;
-  type:TypeOptions;
-}
-//mapping status code to type and color
-const statusToastMap = (status: number): StatusToastConfig => {
-  if (status >= 200 && status < 300) return { type: 'success', color: '#289e43ff' }; // verde
-  if (status >= 400 && status < 500) return { type: 'error', color: '#dc3545' };   // rojo
-  if (status >= 500) return { type: 'warning', color: '#ffc107' };                 // amarillo
-  return { type: 'default', color: '#17a2b8' }; // info
-};
-
-export const showToastByStatus = (
-  message: string,
-  status: number,
-  options?: ToastOptions
-) => {
-  const { type, color } = statusToastMap(status);
-
-  toast(message, {
-    type,
-    style: { backgroundColor: color, color: '#fff' },
-    icon: false,
-    ...options,
-  });
-};
