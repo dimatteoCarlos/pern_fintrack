1baa0ad64a38fbd14494c4793446fd1799290c74
CarlosDR <dimatteoca@gmail.com>
2025-10-05 11:27:35 -0400

    LogoMenuIcon includes logout button

--- frontend/src/general_components/header/LogoMenuIcon.tsx
+++ frontend/src/general_components/header/LogoMenuIcon.tsx
@@ -1,13 +1,31 @@
+//src/general_components/header/LogoMenuIcon.tsx
 import { Link, useLocation } from 'react-router-dom';
 import Logo from '../../assets/logo.svg';
-
 import MenuIcon from '../../assets/MenuSvg.svg';
+import SignOutIcon from '../../assets/SignOutSvg.svg';
 import './logoMenuIcon.css';
+import useAuth from '../../auth/hooks/useAuth';
 function LogoMenuIcon() {
   const { pathname } = useLocation();
+
+//--auth states-------------
+const {handleSignOut, clearError,
+clearSuccessMessage} = useAuth()  
+
+//handler for sign out click function button
+const handleSignOutClick = ()=>{
+  clearError()
+  clearSuccessMessage()
+  handleSignOut()
+  //re direct to /auth and signout
+}
+
   return (
-    <div className='header__logoAndIcon'>
+    <div className='header__logoAndIcon '>
       <Logo />
+      <div className="menuBox"
+      style={{display:'flex', justifyContent:'space-around', width:'25%', }}
+      >
       <Link
         to='accounting'
         className='iconContainer'
@@ -15,6 +33,16 @@ function LogoMenuIcon() {
       >
         <MenuIcon />
       </Link>
+
+      <button
+        onClick={handleSignOutClick}
+        className='iconContainer'
+        style={{border:'none', }}
+       >
+        <SignOutIcon />
+      </button>
+      </div>
+
     </div>
   );
 }
