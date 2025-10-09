c994e84e0345dcdb86ae2155f74bb775f32f008e
CarlosDR <dimatteoca@gmail.com>
2025-10-09 18:09:12 -0400

    index routes for auth and user verify

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,8 @@
+//backend/src/routes/index.js
 import express from 'express';
 import authRoutes from './authRoutes.js';
 import userRoutes from './userRoutes.js';
-import accountRoutes from './accountRoutes.js';
-import transactionRoutes from './transactionRoutes.js'
-import { authDetectClienttype } from '../middlewares/authDetectClientType.js';
+import { authDetectClienttype } from '../middlewares/authDetectClienttype.js';
 
 const router = express.Router(); 
 console.log('index routes');
@@ -11,10 +10,6 @@ console.log('index routes');
 router.use('/auth', authDetectClienttype,authRoutes);
 
 router.use('/user', userRoutes);
-
-router.use('/account', accountRoutes);
-
-router.use('/transaction', transactionRoutes);
 
 export default router;
 
