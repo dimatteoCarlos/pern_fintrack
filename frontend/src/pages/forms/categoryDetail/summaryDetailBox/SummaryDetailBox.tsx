38a2d4347187bd261069efad133495dc8a487f93
CarlosDR <dimatteoca@gmail.com>
2025-10-26 13:11:08 -0400

    SummaryDetailBox

--- frontend/src/pages/forms/categoryDetail/summaryDetailBox/SummaryDetailBox.tsx
+++ frontend/src/pages/forms/categoryDetail/summaryDetailBox/SummaryDetailBox.tsx
@@ -1,7 +1,7 @@
 // SummaryDetailBox.tsx
 import { StatusSquare } from "../../../../general_components/boxComponents";
 import { DEFAULT_CURRENCY } from "../../../../helpers/constants";
-import { getCurrencySymbol, numberFormatCurrency } from "../../../../helpers/functions";
+import { digitRound, getCurrencySymbol, numberFormatCurrency } from "../../../../helpers/functions";
 import { CurrencyType } from "../../../../types/types";
 import './styles/summaryDetailBox-style.css'
 
@@ -41,17 +41,21 @@ function SummaryDetailBox({ bubleInfo }:SummaryDetailPropType ) {
           <div className='summary__data--amount'>
             <span> {getCurrencySymbol(currency_code??defaultCurrency)}</span>
             <span>
-            {amount}
+            {digitRound(amount)}
              </span>
           </div>
 
-          <div className='summary__data--subtitle1'>{subtitle1} {numberFormatCurrency(amount1, 2,currency_code)}&nbsp;({(amount !==0 &&amount1 ? Math.abs(amount1!/(amount)*100):0).toFixed(1)}%)</div>
+          <div className='summary__data--subtitle1'>{subtitle1} {numberFormatCurrency(amount1, 1,currency_code)}&nbsp;({(amount !==0 && amount1 ? Math.abs(amount1!/(amount)*100):0).toFixed(1)}%)</div>
 
           <div className='summary__data--status '>
             {/* status: */}
             <StatusSquare alert={status? 'alert' : ''}/>
             {/* <StatusSquare alert={amount2 >0 ? 'alert' : ''}/> */}
-            <div className='summary__data--subtitle2'>{(amount !==0 && amount2 ? Math.abs(amount2!/(amount)*100):0).toFixed(1)}%</div>
+            <div className='summary__data--subtitle2'>{(amount !==0 && amount2 
+              ? 100-Math.abs(amount1!/(amount)*100)
+              // Math.abs(amount2!/(amount)*100)
+            :0
+            ).toFixed(1)}%</div>
           </div>
         </div>
       </div>
