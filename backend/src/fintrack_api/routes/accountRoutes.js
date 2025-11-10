86eb499948f6de7df01d6e3e0e8b6a27c465b00a
CarlosDR <dimatteoca@gmail.com>
2025-11-10 18:32:14 -0400

    new routes for edition

--- backend/src/fintrack_api/routes/accountRoutes.js
+++ backend/src/fintrack_api/routes/accountRoutes.js
@@ -1,36 +1,49 @@
 //backend\src\fintrack_api\routes\accountRoutes.js
-
-//New Account Creation routes
+//----------------------------
+//RULES
+//tracker movements - type accounts consist on:
+//expense: bank and category_budget account types
+//income: bank and income_source_accounts
+//investment: investment_accounts
+//pocket_saving: pocket_saving_accounts
+//debtor: debtor_accounts
+//------------------------------
 import express from 'express';
-//controllers
+// 📚 ACCOUNT CREATION CONTROLLERS (basic (bank, investment, income), debtor, pocket)
 import {
   createBasicAccount,
   createDebtorAccount,
   createPocketAccount,
 } from '../controllers/accountCreationController.js';
 
+// 📚 ACCOUNT READING CONTROLLERS
 import {
   getAccounts,
   getAllAccountsByType,
   getAccountById,
   getAccountsByCategory,
-  // getCategoryBudgetFullDataEndpoint,
+// getCategoryBudgetFullDataEndpoint,
 } from '../controllers/getAccountController.js';
 
+// 📚 CATEGORY_BUDGET ACCOUNT Creation Controller
 import { createCategoryBudgetAccount } from '../controllers/accountCategoryCreationcontroller.js';
 
+// 📚 TRANSACTIONS CONTROLLER
 import { getTransactionsForAccountById } from '../controllers/getTransactionsForAccountById.js';
 // import { verifyToken } from '../../middlewares/authMiddleware.js';
 
-// import { verifyHeaderAuth, verifyUser } from '../middlewares/authMiddleware.js';
-// router.post('/new_account/bank',verifyUser ,createBasicAccount);
-
+// 📚 ACCOUNT EDITION CONTROLLER
+import {patchAccountById} from '../controllers/accountEditController.js';
+//----------------------------------
+// ROUTES
+//----------------------------------
 const router = express.Router();
-
-//CREATE ACCOUNTS BY ACCOUNT TYPE
+// ---------------------------------
+// 📝 CREATE ACCOUNT ROUTES
+// ---------------------------------
 router.post('/new_account/bank',
    //verifyToken,
-  createBasicAccount); //bank
+  createBasicAccount); 
 
 router.post('/new_account/income_source',
   // verifyToken,
@@ -51,16 +64,27 @@ router.post('/new_account/pocket_saving',
 router.post('/new_account/category_budget',
   //  verifyToken,
   createCategoryBudgetAccount);
-//----------------------------
-//RULES
-//tracker movements - type accounts involved
-//expense: bank and category_budget account types
-//income: bank and income_source_accounts
-//investment: investment_accounts
-//pocket_saving: pocket_saving_accounts
-//debtor: debtor_accounts
+
+// ---------------------------------
+// 🛠️ ACCOUNT EDITION ROUTES
+// ---------------------------------
+//Route for getting account details info for edition form
+// GET /api/fintrack/account/details/:accountId
+router.get('/details/:accountId', 
+  getAccountById // RE-USED
+);
+
+// Route for partially update an existing account
+// PATCH /api/fintrack/account/edit/:accountId
+router.patch('/edit/:accountId',
+patchAccountById 
+);
+
 //------------------------------
 //GET USER ACCOUNT INFO BY TYPE, BY ID, ALL ACC.
+// ---------------------------------
+// 📖 ACCOUNT READING (GET) ROUTES
+// ---------------------------------
 router.get('/allAccounts',
   // verifyToken, 
   getAccounts);
@@ -72,19 +96,24 @@ router.get('/type',
 router.get('/:accountId',
   // verifyToken,
    getAccountById);
-   
+
+// ---------------------------------
+// 📖 ACCOUNT READING (GET) ROUTES
+// ---------------------------------
 router.get('/transactions/:accountId',
   // verifyToken, 
   getTransactionsForAccountById);
 
-//GET USER ACCOUNT LIST INFO BY CATEGORY NAME
+// ---------------------------------
+// 📝 GET USER ACCOUNT LIST INFO BY CATEGORY NAME
+// ---------------------------------
 // get all category budget account type info associated to a category Name
-
 //route: /api/fintrack/category/
 router.get('/category/:categoryName',
   // verifyToken, 
   getAccountsByCategory);
 
+//----------------------
 //  router.get('/:accountId/category-budget-full', getCategoryBudgetFullDataEndpoint); 
 //----------------------
 export default router;
