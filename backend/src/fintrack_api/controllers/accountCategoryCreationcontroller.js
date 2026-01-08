56d26dc2571790055bc394fbe275f016fffd036f
CarlosDR <dimatteoca@gmail.com>
2026-01-08 17:07:24 -0400

    fixed utils import

--- backend/src/fintrack_api/controllers/accountCategoryCreationcontroller.js
+++ backend/src/fintrack_api/controllers/accountCategoryCreationcontroller.js
@@ -4,18 +4,18 @@ import pc from 'picocolors';
 import {
   createError,
   handlePostgresError,
-} from '../../../utils/errorHandling.js';
+} from '../../utils/errorHandling.js';
 import { pool } from '../../db/configDB.js';
-import { determineTransactionType, formatDate } from '../../../utils/helpers.js';
-import { recordTransaction } from '../../../utils/recordTransaction.js';
-import { checkAndInsertAccount } from '../../../utils/checkAndInsertAccount.js';
-import { verifyAccountExistence } from '../../../utils/verifyAccountExistence.js';
-import { updateAccountBalance } from '../../../utils/updateAccountBalance.js';
-import { insertAccount } from '../../../utils/insertAccount.js';
-import { getTransactionTypeId } from '../../../utils/getTransactionTypeId.js';
-import { determineSourceAndDestinationAccounts } from '../../../utils/determineSourceAndDestinationAccounts.js';
-import { prepareTransactionOption } from '../../../utils/prepareTransactionOption.js';
-//import { validateAndNormalizeDate } from '../../../utils/helpers.js';
+import { determineTransactionType, formatDate } from '../../utils/helpers.js';
+import { recordTransaction } from '../../utils/recordTransaction.js';
+import { checkAndInsertAccount } from '../../utils/checkAndInsertAccount.js';
+import { verifyAccountExistence } from '../../utils/verifyAccountExistence.js';
+import { updateAccountBalance } from '../../utils/updateAccountBalance.js';
+import { insertAccount } from '../../utils/insertAccount.js';
+import { getTransactionTypeId } from '../../utils/getTransactionTypeId.js';
+import { determineSourceAndDestinationAccounts } from '../../utils/determineSourceAndDestinationAccounts.js';
+import { prepareTransactionOption } from '../../utils/prepareTransactionOption.js';
+//import { validateAndNormalizeDate } from '../../utils/helpers.js';
 //-----------------
 //endpoint: POST: http://localhost:5000/api/fintrack/account/new_account/category_budget?user=6e0ba475-bf23-4e1b-a125-3a8f0b3d352c
 //rules
