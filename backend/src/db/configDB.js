218d16d11be9bd835b0ba21565f9103f81e282a4
CarlosDR <dimatteoca@gmail.com>
2026-01-13 13:32:45 -0400

    changed confi to client and verify production env

--- backend/src/db/configDB.js
+++ backend/src/db/configDB.js
@@ -3,9 +3,13 @@ import pc from 'picocolors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 const uri = {
   connectionString: process.env.DATABASE_URI,
   //,  ssl: { rejectUnauthorized: false }, //Never use this in production. alternative: mkcert
+  ssl: isProduction ? { rejectUnauthorized: false } : false,
 
   connectionTimeoutMillis: 10000, // Tiempo de espera para la conexión (5 segundos)
 
@@ -14,16 +18,26 @@ const uri = {
 
 export const pool = new pg.Pool(uri); // Ojo, Pool espera un objeto
 
+// Manejo de errores globales del pool (Muy recomendado)
+pool.on('error', (err) => {
+  console.error(pc.red('Error inesperado en un cliente inactivo de la DB:'), err);
+  // No cerramos el proceso necesariamente, pero lo registramos
+});
+
 export async function checkConnection() {
   try {
-    await pool.query('SELECT 1');
+   const client = await pool.connect();
+    await client.query('SELECT 1');
+    client.release();
+
     console.log(
       pc.italic(pc.yellowBright('Conexión a la base de datos verificada.'))
     ); //Data base connection verified
   } catch (error) {
     console.error(
-      pc.red('Error al verificar la conexión a la base de datos:', error)
+      pc.red('❌ Error crítico al conectar con la base de datos:', error.message)
     ); //Error when connecting to data base.
+    // if (isProduction) throw error;
     throw error;
   }
 }
