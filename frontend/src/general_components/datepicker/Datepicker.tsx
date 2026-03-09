010e968e3c63aed4cef495a3c7c6ec7816c33165
CarlosDR <dimatteoca@gmail.com>
2026-03-09 11:33:32 -0500

    feat:datepicker fixed data input now text is not acceptable

--- frontend/src/general_components/datepicker/Datepicker.tsx
+++ frontend/src/general_components/datepicker/Datepicker.tsx
@@ -1,18 +1,19 @@
-//TrackerDatePicker.tsxDatepicker
-// import { useEffect, useState } from 'react';
+// frontend/src/general_components/datepicker/Datepicker.tsx
+
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import './styles/datepicker-styles.css';
 //-------
 // import { showDate } from '../../helpers/functions';
 import { VariantType } from '../../types/types';
+import React from 'react';
 
 //to express date in spanish
 // import es  from 'date-fns/locale/es';
 // import { registerLocale } from 'react-datepicker';
 // registerLocale('es', es, );
-//-------
 
+//-------
 type DatePickerProps = {
   date: Date;
   changeDate: (selectedDate: Date) => void;
@@ -20,6 +21,26 @@ type DatePickerProps = {
   isReset?: boolean;
 };
 
+//-------------------------------------
+// 🧩 Custom read-only input
+//react-datepicker recommends the usage of forwardRef to avoid focus problems.
+const ReadOnlyInput = React.forwardRef<
+ HTMLInputElement,
+ React.InputHTMLAttributes<HTMLInputElement>
+>((props, ref) => {
+
+ return (
+  <input
+   {...props}
+   ref={ref}
+   readOnly
+   inputMode="none"
+   onPaste={(e) => e.preventDefault()}
+  />
+ )
+
+ })
+//--------------------
 function Datepicker({
   date ,
   changeDate,
@@ -29,12 +50,20 @@ function Datepicker({
  return (
   <DatePicker
    selected={date}
-   onChange={(date) => changeDate(date??new Date())}
+   // onChange={(date) => changeDate(date??new Date())}
+
+   onChange = {(date)=>{if(!date) return
+   changeDate(date)}}
+
    showYearDropdown
    scrollableMonthYearDropdown
    placeholderText='DD/MM/YYYY'
    dateFormat='dd/MMM/yyyy'
-   customInput={<input inputMode='none' />} //not showing mobile keyboard
+
+  customInput={<ReadOnlyInput />}
+
+  // customInput={<input readOnly inputMode='none' />} //not showing mobile keyboard
+
    className={
      variant == 'tracker' || variant == 'light'
        ? 'tracker__inside__datepicker'
