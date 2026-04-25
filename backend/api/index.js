04386eeb3d5c06d1ff9094da8e50cf8276495a1c
CarlosDR <dimatteoca@gmail.com>
2026-04-25 18:07:35 -0500

    feat(deploy): serverless express handler in api/index.js

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,10 @@
 // backend/api/index.js
-// import app from '../src/app.js';
-// import serverless from 'serverless-http';
+import app from '../src/app.js';
+import serverless from 'serverless-http';
 
-// export default serverless(app);
-export default function handler(req, res) {
-  // res.status(200).json({ from: "test" });
-   res.status(200).send("Hello from Vercel");
-}
+export default serverless(app);
+// export default function handler(req, res) {
+//   // res.status(200).json({ from: "test" });
+//    res.status(200).send("Hello from Vercel");
+// }
 
