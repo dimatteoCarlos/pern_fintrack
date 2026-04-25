fc89b740c4764706a5c03799fccbbd08b154ad66
CarlosDR <dimatteoca@gmail.com>
2026-04-25 18:52:38 -0500

    feat(deploy):new test with handler in api index.js?

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,10 +1,11 @@
 // backend/api/index.js
-import app from '../src/app.js';
-import serverless from 'serverless-http';
+// import app from '../src/app.js';
+// import serverless from 'serverless-http';
 
-export default serverless(app);
-// export default function handler(req, res) {
-//   // res.status(200).json({ from: "test" });
-//    res.status(200).send("Hello from Vercel");
-// }
-
+export default function handler(req, res) {
+  res.status(200).json({
+    from: 'api index',
+    method: req.method,
+    url: req.url,
+  });
+}
