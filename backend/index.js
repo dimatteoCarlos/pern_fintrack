5e304c12f3dc13e39fcf71645e4342ef2f2bc45a
CarlosDR <dimatteoca@gmail.com>
2026-04-29 17:31:21 -0500

    fix: remove handler wrapper for vercel

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,12 @@
 //backend/index.js
 import app from './src/app.js';
-
-export default async function handler(req, res) {
-  // Ejecuta Express con los objetos req/res de Vercel
-  await new Promise((resolve, reject) => {
-    app(req, res, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
-}
+export default app;
+// export default async function handler(req, res) {
+//   // Ejecuta Express con los objetos req/res de Vercel
+//   await new Promise((resolve, reject) => {
+//     app(req, res, (err) => {
+//       if (err) reject(err);
+//       else resolve();
+//     });
+//   });
+// }
