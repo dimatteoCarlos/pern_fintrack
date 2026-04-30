52ed9c7521fe7ab2e4a9f886e85bd073f79a920e
CarlosDR <dimatteoca@gmail.com>
2026-04-30 12:52:56 -0500

    after successful test. now testing app handler as a promise

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,19 @@
 //backend/index.js
 import app from './src/app.js';
 
-export default function handler(req, res) {
-  return app(req, res);
-}
-
-// export default async function handler(req, res) {
-  // Ejecuta Express con los objetos req/res de Vercel
-//   await new Promise((resolve, reject) => {
-//     app(req, res, (err) => {
-//       if (err) reject(err);
-//       else resolve();
-//     });
-//   });
+// export default function handler(req, res) {
+//   return app(req, res);
 // }
+
+// Ejecuta Express con los objetos req/res de Vercel
+export default async function handler(req, res) {
+  await new Promise((resolve, reject) => {
+    app(req, res, (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
 
 //This structure work ok
 // export default function handler(req, res) {
