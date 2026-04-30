64a0c7cb0f0bbbf0630612f08d8f77e56da0b2de
CarlosDR <dimatteoca@gmail.com>
2026-04-30 10:26:38 -0500

    test: direct handler without express to validate vercel execution

--- backend/index.js
+++ backend/index.js
@@ -5,12 +5,16 @@ import app from './src/app.js';
 //   return app(req, res);
 // }
 
-export default async function handler(req, res) {
+// export default async function handler(req, res) {
   // Ejecuta Express con los objetos req/res de Vercel
-  await new Promise((resolve, reject) => {
-    app(req, res, (err) => {
-      if (err) reject(err);
-      else resolve();
-    });
-  });
+//   await new Promise((resolve, reject) => {
+//     app(req, res, (err) => {
+//       if (err) reject(err);
+//       else resolve();
+//     });
+//   });
+// }
+
+export default function handler(req, res) {
+  res.status(200).json({ message: "DIRECT_HANDLER_OK" });
 }
