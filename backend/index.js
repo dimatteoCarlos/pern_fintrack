d57c81b4de4f7596f67b3a78edd7390cd0c298fe
CarlosDR <dimatteoca@gmail.com>
2026-04-29 20:57:10 -0500

    fix handler correct as original

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,14 @@
 //backend/index.js
 import app from './src/app.js';
-export default function handler(req, res) {
-  return app(req, res);
-}
-// export default async function handler(req, res) {
-//   // Ejecuta Express con los objetos req/res de Vercel
-//   await new Promise((resolve, reject) => {
-//     app(req, res, (err) => {
-//       if (err) reject(err);
-//       else resolve();
-//     });
-//   });
+// export default function handler(req, res) {
+//   return app(req, res);
 // }
+export default async function handler(req, res) {
+  // Ejecuta Express con los objetos req/res de Vercel
+  await new Promise((resolve, reject) => {
+    app(req, res, (err) => {
+      if (err) reject(err);
+      else resolve();
+    });
+  });
+}
