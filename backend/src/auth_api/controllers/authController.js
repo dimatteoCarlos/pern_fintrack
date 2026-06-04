2b46e25586206c44a9d5922d5e1a3446a19eb90d
CarlosDR <dimatteoca@gmail.com>
2026-06-03 19:29:10 -0500

    refactor(auth): use centralized getCurrencyId from currencyLookup

--- backend/src/auth_api/controllers/authController.js
+++ backend/src/auth_api/controllers/authController.js
@@ -19,7 +19,9 @@ import {
 } from '../../utils/authUtils/cookieConfig.js';
 import { createError } from '../../utils/errorHandling.js';
 
-import { getCurrencyId } from '../../fintrack_api/controllers/transactionController.js';
+import { getCurrencyId } from '../../utils/currencyLookup.js';
+
+// import { getCurrencyId } from '../../fintrack_api/controllers/transactionController.js';
 
 //=====================
 //FUNCTIONS DECLARATION
@@ -94,6 +96,7 @@ export const signUpUser = async (req, res, next) => {
     const newUserId = uuidv4();
     //In db tabl. currency_id = 1. currency_code= 'usd'
     const currencyId = !currency ? 1 : await getCurrencyId(client, currency);
+    
     console.log('🚀 ~ signUpUser ~ currencyId:', currencyId);
     // console.log('hashedPwd:', hashedPassword.length);
     // console.log('testUUID:', newUserId);
